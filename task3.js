1d3b5aecb11e8c4264970a96f6b4e51fea6174cd
Ivan Zharikov <izharikov888@gmail.com>
2022-10-14 11:12:58 -0400

    Second commit

--- task3.js
+++ task3.js
@@ -3,33 +3,40 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 let n = Math.floor(Math.random() * 100);
-let tries = 0; 
 
 
-const p = new Promise(function(resolve, reject) {
-    setTimeout(() => {
-        resolve(console.log('Guess a number from 1 to 100: '))
-    })
-})
+async function getUserInput() {
+    let promise = new Promise(function(resolve, reject) {
+        rl.question('Guess a number from 1 to 100: ', (input) => {
+            let number = input;
+            rl.pause();
+            return resolve(number); 
+            
+        });  
+    });
+    return await promise;
+}
 
+async function game() {
+    let tries = 0;
+    while(true) {
+        let input = await getUserInput();
+        let userNumber = +input
+        tries++;
+    
+        if(userNumber === n) {
+            console.log(`Congrats! You guessed with ${ tries } attempts!`);
+            break;
+        }
+    
+        if(userNumber > n) {
+            console.log(`Incorrect, you used ${ tries } attempts, please try again, number is smaller!\n`);
+        } 
+        if(userNumber < n) {
+            console.log(`Incorrect, you used ${ tries } attempts, please try again, number is bigger!\n`);
+        }
+    }
+    rl.close();
+}
 
-p.then(() => {
-    return new Promise(function(resolve, reject) {
-        rl.on('line', (userInput) => {
-            tries++
-            if (userInput < n)
-                console.log(`Incorrect, you used ${ tries } attempts, please try again, number is bigger!\n`);
-                rl.prompt()
-            if (userInput > n)
-                console.log(`Incorrect, you used ${ tries } attempts, please try again, number is smaller!\n`);
-                rl.prompt()
-            if (userInput == n)
-                rl.close();
-        resolve();
-        })
-    })
-}).then(() => {
-    rl.on('close', () => {
-        console.log(`Congrats! You guessed with ${ tries } attempts!`)
-    })
-});
+game();
